238384a5216dc884615922816e79af986b836914
Dumitru Bolandau <109096945+DumitruBolandau78@users.noreply.github.com>
2022-11-12 23:54:41 +0200

    update

--- js/index.js
+++ js/index.js
@@ -24,3 +24,25 @@ function modalCreditCardClose() {
     document.getElementById('body').style.overflowY = 'scroll';
     document.getElementById("modal-credit-card").style.display = 'none';
 }
+
+function submit(){
+    let fullName = document.getElementById('fullName').value;
+    let number = document.getElementById('number').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+
+    if (fullName === '' || number === '' || email === '' || message === ''){
+        alert('Please fill out the form!')
+    }
+    else {
+        alert('Thank you for your request')
+        document.getElementById('fullName').value = '';
+        document.getElementById('number').value = '';
+        document.getElementById('email').value = '';
+        document.getElementById('message').value = '';
+        document.getElementById("i3").style.display = 'none';
+        document.getElementById('body').style.overflowY = 'scroll';
+        document.getElementById("modal-credit-card").style.display = 'none';
+        document.getElementById('MasterCard').selected = 'selected';
+    }
+}
