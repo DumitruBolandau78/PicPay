9f668b67c0fc82aec5a5453bc0f8838c47adfa45
Dumitru Bolandau <109096945+DumitruBolandau78@users.noreply.github.com>
2022-11-12 22:24:08 +0200

    update

--- js/index.js
+++ js/index.js
@@ -9,3 +9,18 @@ function asideClose() {
     document.getElementById("i2").style.display = 'none';
     document.getElementById("i1").style.display = 'block';
 }
+
+function modalCreditCardOpen() {
+    document.getElementById('modal-credit-card').style.position = 'fixed';
+    document.getElementById('body').style.overflowY = 'hidden';
+    document.getElementById('modal-credit-card').style.display = 'flex';
+    document.getElementById('modal-credit-card').style.justifyContent = 'center';
+    document.getElementById('modal-credit-card').style.alignItems = 'center';
+    document.getElementById("i3").style.display = 'block';
+}
+
+function modalCreditCardClose() {
+    document.getElementById("i3").style.display = 'none';
+    document.getElementById('body').style.overflowY = 'scroll';
+    document.getElementById("modal-credit-card").style.display = 'none';
+}
